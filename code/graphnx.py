30ab6485c0636c33d2d1f029fb663318f3e95700
Zhongpeng Lin <lin.zhp@gmail.com>
2015-01-28 10:19:44 -0800

    Implemented triangle counting using wedge iteration with degeneracy ordering.
    python code\graphnx.py graphs\email-Enron.txt
    Number of directed edges: 183831
    Number of triangles: 43454
    Time used: 394.46024203300476

--- code/graphnx.py
+++ code/graphnx.py
@@ -3,7 +3,10 @@ __author__ = 'Zhongpeng Lin'
 Graph algorithms implemented with networkx
 """
 from networkx import Graph, DiGraph
-import sys,re
+import sys
+import re
+import itertools
+import time
 
 
 def read_graph(file):
@@ -45,14 +48,40 @@ def get_least_degree_node(graph):
     least_degree_node = None
     least_degree = graph.number_of_nodes()
     for node, degree in graph.degree_iter():
-        if degree == 1:
+        if degree <= 2:
             return node
         elif degree < least_degree:
             least_degree = degree
             least_degree_node = node
     return least_degree_node
 
+
+def degeneracy_wedge_iterator(graph):
+    """
+    The graph will be destroyed while iteration
+    :param graph:
+    :yield: wedges
+    """
+    while graph.number_of_nodes() > 0:
+        node = get_least_degree_node(graph)
+        successors = graph.successors(node)
+        for pair in itertools.combinations(successors, 2):
+            yield (node, pair)
+        graph.remove_node(node)
+
+
+def count_triangle_degeneracy_wedge_iteration(graph):
+    n = 0
+    for wedge in degeneracy_wedge_iterator(graph):
+        if graph.has_edge(wedge[1][0], wedge[1][1]):
+            n += 1
+    return n
+
 if __name__ == '__main__':
     g = read_dag(sys.argv[1])
     print('Number of directed edges:', g.number_of_edges())
-    print('Least degree:', g.degree(get_least_degree_node(g)))
+    n = 0
+    start_time = time.time()
+    n = count_triangle_degeneracy_wedge_iteration(g)
+    print('Number of triangles:', n)
+    print('Time used:', time.time() - start_time)
